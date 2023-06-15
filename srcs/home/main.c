f5e4df5b15703eba61f67e22eecf0d804bfc4eb8
Abdelhalim El Ouardy <abdeel-o@c1r6p8.1337.ma>
2023-06-15 17:13:37 +0100

    repartition

--- srcs/home/main.c
+++ srcs/home/main.c
@@ -6,44 +6,21 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:45:03 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/06/15 16:19:30 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:04:45 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void    _perror(char *type, char *msg)
-{
-    if (!type && !msg)
-        return ;
-    if (type)
-        ft_putstr_fd(type, 2);
-    ft_putstr_fd(": ", 2);
-    if (msg)
-        ft_putstr_fd(msg, 2);
-}
-
-int check_extension(char *ext)
-{
-    int l;
-
-    if (!ext)
-        return (EXIT_FAILURE);
-    l = ft_strlen(ext);
-    if (l < 4)
-        return (EXIT_FAILURE);
-    ext = ext + (l - 4);
-    if (!ft_strncmp(ext, ".cub", 4))
-        return (EXIT_SUCCESS);
-    return (EXIT_FAILURE);
-}
-
 int main(int ac, char **av)
 {
-    (void)av;
+    t_config    config;
+
     if (ac != 2)
         return (_perror("Usage", USAGE), 1);
     if (check_extension(av[1]))
         return (_perror("Map", MAP), 1);
+    if (_parser(av[1], &config)
+        return (EXIT_FAILURE);
     return (EXIT_SUCCESS);
 }
