4a2805bfcfbbf35b7911d15b51e64a292a3fce10
Abdelhalim El Ouardy <abdeel-o@c1r6p8.1337.ma>
2023-06-15 17:12:09 +0100

    add project structures and helper functions prototype

--- include/CUB3D/cub3d.h
+++ include/CUB3D/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:49:20 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/06/15 16:10:55 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/06/15 17:11:24 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,38 @@
 
 #define USAGE "./cub3D <PATH>/map.cub"
 #define MAP   "Invalid file extension"
+
+typedef struct	s_map
+{
+	int		width;
+	int		height;
+	char	**grid;
+}				t_map;
+
+typedef struct	s_textures
+{
+	char    *north;
+	char	*south;
+	char	*east;
+	char	*west;
+}				t_textures;
+
+typedef struct	s_colors
+{
+	int		floor;
+	int		ceiling;
+}				t_colors;
+
+typedef struct	s_config
+{
+	t_textures	textures;
+	t_colors	colors;
+	t_map		map;
+}				t_config;
+
+
+void    _perror(char *type, char *msg);
+int     check_extension(char *ext);
+
 
 #endif
