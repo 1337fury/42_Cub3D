86476089a2c8a6582f6b01295f97d7731a5a9178
1337fury <elouardiabdelhalim@gmail.com>
2023-08-01 15:29:19 +0100

    create costum texture buffer

--- srcs/engine/engine.c
+++ srcs/engine/engine.c
@@ -6,11 +6,69 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 11:10:39 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/07/31 20:51:22 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/08/01 13:39:12 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	_init_buffers(t_game_tex *g_tex, t_gc *gc)
+{
+	size_t	count;
+
+	count = TEXTURE_HEIGHT * TEXTURE_WIDTH;
+	g_tex->n_buffer = _memory(count, sizeof(size_t), gc);
+	if (!g_tex->n_buffer)
+		return (EXIT_FAILURE);
+	g_tex->s_buffer = _memory(count, sizeof(size_t), gc);
+	if (!g_tex->s_buffer)
+		return (EXIT_FAILURE);
+	g_tex->w_buffer = _memory(count, sizeof(size_t), gc);
+	if (!g_tex->w_buffer)
+		return (EXIT_FAILURE);
+	g_tex->e_buffer = _memory(count, sizeof(size_t), gc);
+	if (!g_tex->e_buffer)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
+
+int	fill_buffer(size_t *buffer, uint8_t* pix)
+{
+	unsigned long	i;
+	unsigned long	j;
+	unsigned long	size;
+
+	if (!buffer || !pix)
+		return (EXIT_FAILURE);
+	i = 0;
+	j = 0;
+	size = 4 * (TEXTURE_HEIGHT * TEXTURE_WIDTH);
+	while (i < size)
+	{
+		if (j < (TEXTURE_HEIGHT * TEXTURE_WIDTH))
+			buffer[j] = rgba_to_hex(pix[i], pix[i + 1], pix[i + 2], pix[i + 3]);
+		i += 4;
+		j++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+int	create_costum_texture_buffer(t_game_tex *g_tex, t_gc *gc)
+{
+	if (!g_tex || !gc)
+		return (EXIT_FAILURE);
+	if (_init_buffers(g_tex, gc))
+		return (EXIT_FAILURE);
+	if (fill_buffer(g_tex->n_buffer, g_tex->north->pixels))
+		return (EXIT_FAILURE);
+	if (fill_buffer(g_tex->s_buffer, g_tex->south->pixels))
+		return (EXIT_FAILURE);
+	if (fill_buffer(g_tex->w_buffer, g_tex->west->pixels))
+		return (EXIT_FAILURE);
+	if (fill_buffer(g_tex->e_buffer, g_tex->east->pixels))
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
 
 int	get_game_textures(t_game *g)
 {
@@ -193,7 +251,7 @@ void	game_spirit(void *data)
     if (key_press(game))
         cleanupAndExit("key press", "failed!", game);
 }
-// [GIMI] M9awed√
+
 void rotate_by_mouse(double xpos, double ypos, void* param)
 {
 	t_game* game;
@@ -214,6 +272,17 @@ int game_engine(t_game *game, t_config *conf)
     game->g_conf = *conf;
 	if (get_game_textures(game))
 		return (EXIT_FAILURE);
+	if (create_costum_texture_buffer(&game->g_tex, game->gc))
+		return (EXIT_FAILURE);
+	
+	/*
+		-test
+	*/
+	for(int i = 0; i < (64 * 64); i++)
+	{
+		printf("%zu\n", game->g_tex.n_buffer[i]);
+	}
+
     if (!mlx_loop_hook(game->mlx, (void *)game_spirit, game))
         return (_perror("mlx loop hook", "failed"), 1);
 	// mlx_cursor_hook(game->mlx, rotate_by_mouse, game);
