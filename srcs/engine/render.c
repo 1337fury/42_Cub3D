02968715748ab1786a6f9ebb082cb5db4c8a9993
1337fury <elouardiabdelhalim@gmail.com>
2023-08-12 10:57:52 +0100

    fix the Norm

--- srcs/engine/render.c
+++ srcs/engine/render.c
@@ -18,20 +18,6 @@ float	distance_bet_points(float x1, float y1, float x2, float y2)
 	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
 }
 
-typedef struct s_coor
-{
-	float		xintercept;
-	float		yintercept;
-	float		xstep;
-	float		ystep;
-}	t_coor;
-
-typedef struct s_next_touch
-{
-	float	next_touch_x;
-	float	next_touch_y;
-}	t_next_touch;
-
 t_next_touch	ver_inter(t_coor *coor, t_player *p, t_ray *rays, int id)
 {
 	t_next_touch	points;
@@ -80,90 +66,121 @@ t_next_touch	horz_inter(t_coor *coor, t_player *p, t_ray *rays, int id)
 	return (points);
 }
 
-void	cast(int coloumn_id, t_game *g)
+void	ver_ray_grid_inter(t_game *g, t_all *all, int id)
 {
-	t_player	*p = &g->player;
-	t_ray		*rays = g->rays;
-	t_map		*map = &g->g_conf.map;
-	t_next_touch ver;
-	t_next_touch horz;
-	t_coor	coor;
-	///////////////////////////////////////////
-    // VERTICAL RAY-GRID INTERSECTION CODE
-    ///////////////////////////////////////////
-	bool	found_ver_wall_hit = false;
-	float	ver_wall_hit_x = 0;
-	float	ver_wall_hit_y = 0;
+	t_next_touch *ver;
+	t_hit_info *hit;
+	t_coor		*coor;
 
-	ver = ver_inter(&coor, p, rays, coloumn_id);
-	while (ver.next_touch_x >= 0 && ver.next_touch_x <= WIDTH && ver.next_touch_y >= 0 && ver.next_touch_y <= HEIGHT)
+	ver = &all->ver;
+	hit = &all->ver_hit;
+	coor = &all->coor;
+	while (ver->next_touch_x >= 0 && ver->next_touch_x <= WIDTH 
+		&& ver->next_touch_y >= 0 && ver->next_touch_y <= HEIGHT)
 	{
-		if (map->has_wall(ver.next_touch_x - (rays[coloumn_id].is_ray_facing_left), ver.next_touch_y, map->grid, g))
+		if (g->g_conf.map.has_wall(ver->next_touch_x - (g->rays[id].is_ray_facing_left),
+			ver->next_touch_y, g->g_conf.map.grid, g))
 		{
-			found_ver_wall_hit = true;
-			ver_wall_hit_x = ver.next_touch_x;
-			ver_wall_hit_y = ver.next_touch_y;
+			hit->found_wall_hit = true;
+			hit->wall_hit_x = ver->next_touch_x;
+			hit->wall_hit_y = ver->next_touch_y;
 			break ;
 		}
 		else
 		{
-			ver.next_touch_x += coor.xstep;
-			ver.next_touch_y += coor.ystep;
+			ver->next_touch_x += coor->xstep;
+			ver->next_touch_y += coor->ystep;
 		}
 	}
+}
 
- 	  ///////////////////////////////////////////
-     // HORIZONTAL RAY-GRID INTERSECTION CODE //
-    ///////////////////////////////////////////
-	bool	found_horz_wall_hit = false;
-	float	horz_wall_hit_x = 0;
-	float	horz_wall_hit_y = 0;
+void	horz_ray_grid_inter(t_game *g, t_all *all, int id)
+{
+	t_next_touch *horz;
+	t_hit_info *hit;
+	t_coor		*coor;
 
-	horz = horz_inter(&coor, p, rays, coloumn_id);
-	while (horz.next_touch_x >= 0 && horz.next_touch_x <= WIDTH && horz.next_touch_y >= 0 && horz.next_touch_y <= HEIGHT)
+	horz = &all->horz;
+	hit = &all->horz_hit;
+	coor = &all->coor;
+	while (horz->next_touch_x >= 0 && horz->next_touch_x <= WIDTH 
+		&& horz->next_touch_y >= 0 && horz->next_touch_y <= HEIGHT)
 	{
-		if (map->has_wall(horz.next_touch_x, horz.next_touch_y - (rays[coloumn_id].is_ray_facing_up), map->grid, g))
+		if (g->g_conf.map.has_wall(horz->next_touch_x, horz->next_touch_y
+			 - (g->rays[id].is_ray_facing_up), g->g_conf.map.grid, g))
 		{
-			found_horz_wall_hit = true;
-			horz_wall_hit_x = horz.next_touch_x;
-			horz_wall_hit_y = horz.next_touch_y;
+			hit->found_wall_hit = true;
+			hit->wall_hit_x = horz->next_touch_x;
+			hit->wall_hit_y = horz->next_touch_y;
 			break ;
 		}
 		else
 		{
-			horz.next_touch_x += coor.xstep;
-			horz.next_touch_y += coor.ystep;
+			horz->next_touch_x += coor->xstep;
+			horz->next_touch_y += coor->ystep;
 		}
 	}
-	///////////////////////////////////////////////////////////////////////////
-	// // calculate both horz and ver distances and choose the smallest value/
-	/////////////////////////////////////////////////////////////////////////
-	float	horz_hit_distance;
-	if (found_horz_wall_hit)
-		horz_hit_distance = distance_bet_points(p->x, p->y, horz_wall_hit_x, horz_wall_hit_y);
-	else
-		horz_hit_distance = FLT_MAX;
+}
 
-	float	ver_hit_distance;
-	if (found_ver_wall_hit)
-		ver_hit_distance = distance_bet_points(p->x, p->y, ver_wall_hit_x, ver_wall_hit_y);
+void	_initializer(t_all *all)
+{
+	all->ver.next_touch_x = 0.f;
+	all->ver.next_touch_y = 0.f;
+	all->horz.next_touch_x = 0.f;
+	all->horz.next_touch_y = 0.f;
+	all->ver_hit.found_wall_hit = false;
+	all->ver_hit.wall_hit_x = 0.f;
+	all->ver_hit.wall_hit_y = 0.f;
+	all->horz_hit.found_wall_hit = false;
+	all->horz_hit.wall_hit_x = 0.f;
+	all->horz_hit.wall_hit_y = 0.f;
+	all->coor.xintercept = 0.f;
+	all->coor.yintercept = 0.f;
+	all->coor.xstep = 0.f;
+	all->coor.ystep = 0.f;
+	all->horz_hit_distance = 0;
+	all->ver_hit_distance = 0;
+}
+void	calc_distance(t_all *all, t_player *p)
+{
+	if (all->horz_hit.found_wall_hit)
+		all->horz_hit_distance = distance_bet_points(p->x, p->y, all->horz_hit.wall_hit_x, all->horz_hit.wall_hit_y);
 	else
-		ver_hit_distance = FLT_MAX;
-
-	if (horz_hit_distance < ver_hit_distance)
-	{
-		rays[coloumn_id].wall_hit_x = horz_wall_hit_x;
-		rays[coloumn_id].wall_hit_y = horz_wall_hit_y;
-		rays[coloumn_id].distance = horz_hit_distance;
-	}
-	else
-	{
-		rays[coloumn_id].wall_hit_x = ver_wall_hit_x;
-		rays[coloumn_id].wall_hit_y = ver_wall_hit_y;
-		rays[coloumn_id].distance = ver_hit_distance;
-	}
-	rays[coloumn_id].was_hit_vertical = (ver_hit_distance < horz_hit_distance);
+		all->horz_hit_distance = FLT_MAX;
 	
+	if (all->ver_hit.found_wall_hit)
+		all->ver_hit_distance = distance_bet_points(p->x, p->y, all->ver_hit.wall_hit_x, all->ver_hit.wall_hit_y);
+	else
+		all->ver_hit_distance = FLT_MAX;
+}
+
+void	cast(int id, t_game *g)
+{
+	t_player	*p;
+	t_ray		*rays;
+	t_all		all;
+
+	p = &g->player;
+	rays = g->rays;
+	_initializer(&all);
+	all.ver = ver_inter(&all.coor, p, rays, id);
+	ver_ray_grid_inter(g, &all, id);
+	all.horz = horz_inter(&all.coor, p, rays, id);
+	horz_ray_grid_inter(g, &all, id);
+	calc_distance(&all, p);
+	if (all.horz_hit_distance < all.ver_hit_distance)
+	{
+		rays[id].wall_hit_x = all.horz_hit.wall_hit_x;
+		rays[id].wall_hit_y = all.horz_hit.wall_hit_y;
+		rays[id].distance = all.horz_hit_distance;
+	}
+	else
+	{
+		rays[id].wall_hit_x = all.ver_hit.wall_hit_x;
+		rays[id].wall_hit_y = all.ver_hit.wall_hit_y;
+		rays[id].distance = all.ver_hit_distance;
+	}
+	rays[id].was_hit_vertical = (all.ver_hit_distance < all.horz_hit_distance);
 }
 
 int grid_render(t_game *game)
@@ -254,7 +271,6 @@ int p_update(void *para)
     new_x = p->x + cos(p->rot_angle) * move_step;
     new_y = p->y + sin(p->rot_angle) * move_step;
 	map->has_wall = is_has_wall;
-
     if (!map->has_wall(new_x, new_y, map->grid, game))
     {
         p->x = new_x;
@@ -272,7 +288,5 @@ int player_render(t_game *game)
     p = &game->player;
     p->update = p_update;
     p->update(game);
-	
-    // _circle(game, p->x, p->y, p->raduis,  0xffffffff);
     return (EXIT_SUCCESS);
 }
