e1c2dac39411343c3a4fe3604496205ce9ee20e6
1337fury <elouardiabdelhalim@gmail.com>
2023-08-28 10:31:34 +0100

    fix 2 walls moving issue

--- srcs/engine/render.c
+++ srcs/engine/render.c
@@ -6,7 +6,7 @@
 /*   By: abdeel-o <abdeel-o@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 19:28:41 by abdeel-o          #+#    #+#             */
-/*   Updated: 2023/08/26 12:48:22 by abdeel-o         ###   ########.fr       */
+/*   Updated: 2023/08/27 16:36:26 by abdeel-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,22 +39,26 @@ int	grid_render(t_game *game)
 int	p_update(void *para)
 {
 	t_game		*game;
+	t_map		*map;
 	t_player	*p;
-	double		new_x;
-	double		new_y;
 	double		move_step;
+	double		move_side;
 
 	game = (t_game *)para;
+	map = &game->g_conf.map;
 	p = &game->player;
 	p->rot_angle += p->turn_dir * p->rot_speed;
 	move_step = p->walk_dir * p->move_speed;
-	new_x = p->x + cos(p->rot_angle) * move_step;
-	new_y = p->y + sin(p->rot_angle) * move_step;
-	game->g_conf.map.has_wall = is_has_wall;
-	if (!game->g_conf.map.has_wall(new_x, new_y, game->g_conf.map.grid, game))
+	move_side = p->walk_side * M_PI_2;
+	p->new_x = p->x + cos(p->rot_angle + move_side) * move_step;
+	p->new_y = p->y + sin(p->rot_angle + move_side) * move_step;
+	map->has_wall = is_has_wall;
+	if (!map->has_wall(p->new_x, p->new_y, map->grid, game)
+		&& !map->has_wall(p->x, p->new_y, map->grid, game)
+		&& !map->has_wall(p->new_x, p->y, map->grid, game))
 	{
-		p->x = new_x;
-		p->y = new_y;
+		p->x = p->new_x;
+		p->y = p->new_y;
 	}
 	return (EXIT_SUCCESS);
 }
